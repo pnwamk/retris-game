7e6bb52d2f2525ca7c4c170f41c3b17e5c688e2d
Andrew Kent <pnwamk@gmail.com>
2018-09-11 07:32:06 -0400

    progress
    made some progress for rendering and the model/logic. Stopped
    at the moment to investigate why manually inspecting
    fields of a pair does not prove the predicate properties,
    and why a cond/case with integer equality tests doesn't
    give us coverage proof when we test for each possible integer.

--- model.rkt
+++ model.rkt
@@ -1,66 +1,120 @@
 #lang typed/racket #:with-refinements
 
+;; TODO make `equal?` and `eqv?` and `eq?` have
+;; linear-integer-propositions when #:with-refinements
+;; is on
+
 (require (only-in typed/2htdp/image Image))
 
 (provide (all-defined-out))
 
-(define-type BlockColor (U "light blue"
-                           "cornflowerblue"
-                           "violet"
-                           "goldenrod"
-                           "darkseagreen"
-                           "purple"
-                           "pink"))
+(define-type Z4  (Refine [n : Integer] (<= 0 n 3)))
+(define-type Col (Refine [n : Integer] (<= 1 n 10)))
+(define-type Row (Refine [n : Integer] (<= 1 n 20)))
 
-(define-type Z4  (Refine [n : Natural] (<= n 3)))
-(define-type Z10 (Refine [n : Natural] (<= n 9)))
-(define-type Z20 (Refine [n : Natural] (<= n 19)))
+(define-type Shape (U 'I 'J 'L 'O 'S 'T 'Z))
 
-(struct Loc ([x : Z10] [y : Z20]) #:transparent)
+(struct Block ([shape : Shape]
+               [row : Row]
+               [col : Col]
+               [orientation : Z4])
+  #:transparent)
 
-(struct I ([center : Loc] [orientation : Z4]) #:transparent)
-(struct J ([center : Loc] [orientation : Z4]) #:transparent)
-(struct L ([center : Loc] [orientation : Z4]) #:transparent)
-(struct O ([center : Loc] [orientation : Z4]) #:transparent)
-(struct S ([center : Loc] [orientation : Z4]) #:transparent)
-(struct T ([center : Loc] [orientation : Z4]) #:transparent)
-(struct Z ([center : Loc] [orientation : Z4]) #:transparent)
+(: row? (-> Integer Boolean : Row))
+(define (row? r) (<= 1 r 20))
 
-(define-type Block (U I J L O S T Z))
+(: col? (-> Integer Boolean : Col))
+(define (col? c) (<= 1 c 10))
 
-(: block-color (-> Block BlockColor))
-(define (block-color b)
-  (cond
-    [(I? b) "light blue"]
-    [(J? b) "cornflowerblue"]
-    [(L? b) "violet"]
-    [(O? b) "goldenrod"]
-    [(S? b) "darkseagreen"]
-    [(T? b) "purple"]
-    [(Z? b) "pink"]))
+(: valid-loc? (-> (Pair Integer Integer) Boolean
+                  : (Pairof Row Col)))
+(define (valid-loc? p)
+  (and (row? (car p)) (col? (cdr p))))
+
+(: block-locs (-> Block (U #f (Listof (Pairof Row Col)))))
+(define (block-locs b)
+  (match-define (Block shape row col dir) b)
+  (define-syntax-rule (coords (r c) ...)
+    (let ([l (list (cons (+ row r) (+ col c)) ...)])
+      (if (andmap valid-loc? l)
+          l
+          #f)))
+  (case shape
+    ;; XXXX
+    [(I)
+     (match dir
+       [0 (error "TODO")]
+       [1 (error "TODO")]
+       [2 (error "TODO")]
+       [3 (error "TODO")])]
+    ;; JJJ
+    ;;   J
+    [(J)
+     (match dir
+       [0 (error "TODO")]
+       [1 (error "TODO")]
+       [2 (error "TODO")]
+       [3 (error "TODO")])]
+    ;; LLL
+    ;; L
+    [(L)
+     (match dir
+       [0 (error "TODO")]
+       [1 (error "TODO")]
+       [2 (error "TODO")]
+       [3 (error "TODO")])]
+    ;; OO
+    ;; OO
+    [(O) (coords (0 0) (0 1) (1 0) (1 1))]
+    ;;  SS
+    ;; SS
+    [(S)
+     (match dir
+       [0 (error "TODO")]
+       [1 (error "TODO")]
+       [2 (error "TODO")]
+       [3 (error "TODO")])]
+    ;; TTT
+    ;;  T
+    [(T)
+     (match dir
+       [0 (error "TODO")]
+       [1 (error "TODO")]
+       [2 (error "TODO")]
+       [3 (error "TODO")])]
+    ;; ZZ
+    ;;  ZZ
+    [(Z)
+     (match dir
+       [0 (error "TODO")]
+       [1 (error "TODO")]
+       [2 (error "TODO")]
+       [3 (error "TODO")])]))
+
+
+
+(define-type Board (Immutable-HashTable
+                    Row (Immutable-HashTable
+                         Col Image)))
 
 (struct GameState (;; current block player is controlling
-                   [current : Block]
+                   [block : Block]
                    ;; board locations currently occupied by blocks
-                   [board : (Immutable-HashTable Loc Image)])
+                   [board : Board])
   #:transparent)
 
 
-(: board-ref-image (case->
-                    [-> GameState Loc (U Image #f)]
-                    [->  GameState Z10 Z20 (U Image #f)]))
-(define board-ref-image
-  (case-lambda
-    [(s l) (hash-ref (GameState-board s) l #f)]
-    [(s x y) (hash-ref (GameState-board s) (Loc x y) #f)]))
-
+(: board-ref (-> GameState Row Col (U #f Image)))
+(define (board-ref s r c)
+  (define row (hash-ref (GameState-board s) r #f))
+  (and row (hash-ref row c #f)))
 
 (: occupied? (case->
-              [-> GameState Loc Boolean]
-              [->  GameState Z10 Z20 Boolean]))
+              (-> GameState Row Col Boolean)
+              (-> GameState (Listof (Pairof Row Col)) Boolean)))
 (define occupied?
   (case-lambda
-    [(s l) (and (hash-ref (GameState-board s) l #f) #t)]
-    [(s x y) (and (hash-ref (GameState-board s) (Loc x y) #f) #t)]))
-
+    [(s rcs) (andmap (λ ([p : (Pairof Row Col)]) (occupied? s (car p) (cdr p)))
+                     rcs)]
+    [(s r c) (and (board-ref s r c) #t)]))
 
--- render.rkt
+++ render.rkt
@@ -1,14 +1,84 @@
-#lang typed/racket ;#:with-refinements
+#lang typed/racket #:with-refinements
 
-(require typed/2htdp/image)
+(require typed/2htdp/image
+         "model.rkt")
 
-
+(define BORDER-SIZE 2)
 (define BLOCK-SIZE 22)
+
+(define base-board-image (empty-scene (+ (* 2 BORDER-SIZE)
+                                         (* 10 BLOCK-SIZE))
+                                      (+ (* 2 BORDER-SIZE)
+                                         (* 20 BLOCK-SIZE))))
+
+(: render-board (-> GameState Image))
+(define (render-board b)
+  (render-current-block
+   (GameState-block b)
+   (render-blocks (GameState-board b)
+                  base-board-image)))
+
+(: render-blocks (-> Board Image Image))
+(define (render-blocks b initial-image)
+  (let do-row : Image ([row : Row 1]
+                       [img : Image initial-image])
+    (define row-hash  (hash-ref b row #f))
+    (cond
+      [(not row-hash)
+       (if (< row 20)
+           (do-row (add1 row) img)
+           img)]
+      [else
+       (let do-col : Image ([col : Col 1]
+                            [img : Image img])
+         (define x (hash-ref row-hash col #f))
+         (let ([img (if x
+                        (render-square img x row col)
+                        img)])
+           (cond
+             [(< col 10)  (do-col (add1 col) img)]
+             [(< row 20) (do-row (add1 row) img)]
+             [else img])))])))
+
+(: render-square (-> Image Image Row Col Image))
+(define (render-square scene img row col)
+  (place-image img (col->img-x col) (row->img-y row) scene))
+
+(: row->img-y (-> Row Real))
+(define (row->img-y r)
+  (+ BORDER-SIZE (* BLOCK-SIZE (- 20 r)) (/ BLOCK-SIZE 2)))
+
+(: col->img-x (-> Col Real))
+(define (col->img-x c)
+  (- (+ BORDER-SIZE (* BLOCK-SIZE c)) (/ BLOCK-SIZE 2)))
+
+(: render-current-block (-> Block Image Image))
+(define (render-current-block b initial-image)
+  initial-image)
+
+(define-type BlockColor (U "light blue"
+                           "cornflowerblue"
+                           "violet"
+                           "goldenrod"
+                           "darkseagreen"
+                           "purple"
+                           "pink"))
+
+(: block-color (-> Block BlockColor))
+(define (block-color b)
+  (case (Block-shape b)
+    [(I) "light blue"]
+    [(J) "cornflowerblue"]
+    [(L) "violet"]
+    [(O) "goldenrod"]
+    [(S) "darkseagreen"]
+    [(T) "purple"]
+    [(Z) "pink"]))
 
 (: block (-> BlockColor Image))
 (define (block color)
   ;; TODO should typecheck without the assert when #:with-refinements is on!!!
-  (overlay (square (assert (- BLOCK-SIZE 2) positive?) 'solid color)
+  (overlay (square (assert (- BLOCK-SIZE BORDER-SIZE) positive?) 'solid color)
            (square BLOCK-SIZE 'solid "white")))
 
 (define CYAN-BLOCK (block "light blue"))
@@ -18,6 +88,10 @@
 (define GREEN-BLOCK (block "darkseagreen"))
 (define PURPLE-BLOCK (block "purple"))
 (define PINK-BLOCK (block "pink"))
+
+
+
+
 
 ;; Board : 10 wide, 20 high
 
@@ -42,3 +116,15 @@
 ;;
 ;; Z : ZZ     (red)
 ;;      ZZ
+
+(render-board (GameState (Block 'I 20 1 0) (hash 1 ((inst hash Col Image)
+                                                    1 CYAN-BLOCK
+                                                    2 BLUE-BLOCK
+                                                    3 CYAN-BLOCK
+                                                    4 BLUE-BLOCK
+                                                    5 CYAN-BLOCK
+                                                    6 BLUE-BLOCK
+                                                    7 CYAN-BLOCK
+                                                    8 BLUE-BLOCK
+                                                    9 CYAN-BLOCK
+                                                    10 BLUE-BLOCK))))
